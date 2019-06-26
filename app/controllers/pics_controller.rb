a5c3ee116f9280b4d8466feb882cb2f088f2fe93
Hasan Karli <karlihasann@gmail.com>
2019-06-26 17:31:53 +0300

    Instagram

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,55 @@
+class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]  
+  before_action :authenticate_user!, expect: [:index, :show]
+    def index
+      @pics = Pic.all.order('created_at DESC')
+    end
+
+    def new
+      @pic = current_user.pics.build
+    end
+    
+    def create
+      @pic = current_user.pics.build(pic_params)
+      
+      if @pic.save
+        redirect_to @pic, notice: "Yes posted"
+      else
+        render 'new'
+      end    
+    end
+
+    def show
+      
+    end
+
+    def edit
+
+    end
+
+    def update
+      if @pic.update(pic_params)
+        redirect_to @pic, notice: "Yes"
+      else
+        render 'edit'
+      end  
+    end
+
+    def destroy
+      @pic.destroy
+        redirect_to root_path
+    end
+    
+    def upvote
+      @pic.upvote_by current_user
+      redirect_to :back
+    end  
+  private
+    def pic_params
+      params.require(:pic).permit(:title, :description, :image)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
+    end
+end
